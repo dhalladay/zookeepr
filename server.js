1b27c1a96b56cce4971ce2941291f439bf4d6fbb
dhalladay <dave.halladay@outlook.com>
2022-01-04 21:13:48 -0700

    update to snapshot

--- server.js
+++ server.js
@@ -1,17 +1,12 @@
-const express = require('express');
-const { animals } = require('./data/animals');
 const fs = require('fs');
 const path = require('path');
-const { animationFrameScheduler } = require('rxjs');
+const express = require('express');
+const { animals } = require('./data/animals');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-//hello
-
-//parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
-//parse incoming JSON data
 app.use(express.json());
 
 function filterByQuery(query, animalsArray) {
@@ -52,8 +47,7 @@ function createNewAnimal(body, animalsArray) {
   fs.writeFileSync(
     path.join(__dirname, './data/animals.json'),
     JSON.stringify({ animals: animalsArray }, null, 2)
-  )
-  //return finished code to post route for response;
+  );
   return animal;
 }
 
@@ -91,10 +85,9 @@ app.get('/api/animals/:id', (req, res) => {
 });
 
 app.post('/api/animals', (req, res) => {
-  // req.body is where our incoming content will be
+  // set id based on what the next index of the array will be
   req.body.id = animals.length.toString();
 
-  //if any data in the req.body is incorrect, send 400 error back
   if (!validateAnimal(req.body)) {
     res.status(400).send('The animal is not properly formatted.');
   } else {
@@ -102,7 +95,6 @@ app.post('/api/animals', (req, res) => {
     res.json(animal);
   }
 });
-
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
 });
